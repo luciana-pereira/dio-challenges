93ac22f37d1848aa6af26d5b31f70a7c1e4eb957
luciana-pereira <luci.lv14@gmail.com>
2022-01-19 20:32:27 -0300

    Initial Challenges performed in java

---
+++ Challenges/Java/01.Repeating_Structures/01.Name_and_Age/Name_and_Age.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno eo segundo
+representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+
+public class NameAndAge {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+	}
+
+}
